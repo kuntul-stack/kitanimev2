858a1341e6373fc308177f3b61b5906408fb94cc
ikuza <97811292+IkuzaDev@users.noreply.github.com>
2025-09-17 17:41:15 +0700

    Update anime.js

--- routes/anime.js
+++ routes/anime.js
@@ -133,35 +133,10 @@ router.get('/:slug/episode/:episode', async (req, res) => {
     }
     if(Object.keys(getEpisodeDetails.steramList).length == 0 || getEpisodeDetails.steramList['720'] == null){
       qlist.push('480');
-      const blogger = await axios.get(getEpisodeDetails.stream_url, {
-        headers: {
-          'Host': 'desustream.info',
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-          'Cache-Control': 'no-cache',
-          'Pragma': 'no-cache',
-          'Upgrade-Insecure-Requests': '1',
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
-          'Sec-Fetch-Dest': 'document',
-          'Sec-Fetch-Mode': 'navigate',
-          'Sec-Fetch-Site': 'none',
-          'Sec-Fetch-User': '?1',
-          'Sec-GPC': '1',
-          'Sec-CH-UA': '"Not)A;Brand";v="8", "Chromium";v="138", "Brave";v="138"',
-          'Sec-CH-UA-Mobile': '?0',
-          'Sec-CH-UA-Platform': '"Windows"',
-          'Connection': 'keep-alive',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Methods': '*',
-          'Access-Control-Allow-Credentials': 'true',
-        },
-      });
-      const $$ = load(blogger.data);
-      const googleVideoUrl = $$('#myIframe').attr('src');
-      modifiedStreamList['480'] = `/stream?url=${googleVideoUrl}`;
+      modifiedStreamList['480'] = `/stream?url=${getEpisodeDetails.stream_url}`;
     }
     if(!modifiedStreamList['480']){
-      modifiedStreamList['480'] = 'aaaaaaaaaaa'
+      modifiedStreamList['480'] = `/stream?url=${getEpisodeDetails.stream_url}`;
     }
     console.log(modifiedStreamList)
     
